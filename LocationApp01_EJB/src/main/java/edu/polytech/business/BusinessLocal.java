63895b8b9afd31740fc374c9047b077267bdfc97
Banalian <64379456+Banalian@users.noreply.github.com>
2022-03-23 15:07:58 +0100

    Chore : removed old file and continued refactoring

--- LocationApp01_EJB/src/main/java/edu/polytech/business/BusinessLocal.java
+++
@@ -1,9 +0,0 @@
-package edu.polytech.business;
-
-import edu.polytech.dao.Dao;
-
-import javax.ejb.Local;
-
-@Local
-public interface BusinessLocal extends Business {
-}
--- LocationApp01_EJB/src/main/java/edu/polytech/business/BusinessRemote.java
+++
@@ -1,9 +0,0 @@
-package edu.polytech.business;
-
-import edu.polytech.dao.Dao;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface BusinessRemote extends Business {
-}
