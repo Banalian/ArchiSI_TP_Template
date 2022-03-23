4cd8f1e013ce32b8e6575014259041796559e43e
Banalian <64379456+Banalian@users.noreply.github.com>
2022-03-23 15:10:19 +0100

    chore : Added LocationDaoImpl

---
+++ LocationApp01_EJB/src/main/java/edu/polytech/location/dao/LocationDaoImpl.java
@@ -0,0 +1,33 @@
+package edu.polytech.location.dao;
+
+import edu.polytech.location.model.LocationBean;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Stateless
+public class LocationDaoImpl implements LocationDao{
+
+    @PersistenceContext(unitName = "LocationEJB")
+    private EntityManager em;
+
+    @Override
+    public void createLocation(LocationBean bean) {
+        em.persist(bean);
+    }
+
+    @Override
+    public List<LocationBean> getLocations() {
+        Query request = em.createQuery("select l from LocationBean l");
+        return  request.getResultList();
+    }
+
+    @Override
+    public LocationBean getLocation(Integer id) {
+        return em.find(LocationBean.class,id);
+    }
+
+}
