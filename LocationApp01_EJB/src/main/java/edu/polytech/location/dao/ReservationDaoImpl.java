883b9115a91a4a0b92e5706dbe022e7384678b23
Banalian <64379456+Banalian@users.noreply.github.com>
2022-03-24 08:39:49 +0100

    Update ReservationDaoImpl.java

--- LocationApp01_EJB/src/main/java/edu/polytech/location/dao/ReservationDaoImpl.java
+++ LocationApp01_EJB/src/main/java/edu/polytech/location/dao/ReservationDaoImpl.java
@@ -1,4 +1,32 @@
 package edu.polytech.location.dao;
 
-public class ReservationDaoImpl {
+import edu.polytech.location.model.ReservationBean;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Stateless
+public class ReservationDaoImpl implements ReservationDao {
+
+    @PersistenceContext(unitName = "LocationEJB")
+    private EntityManager em;
+
+    @Override
+    public void createReservation(ReservationBean bean) {
+        em.persist(bean);
+    }
+
+    @Override
+    public List<ReservationBean> getReservations() {
+        Query request = em.createQuery("select r from ReservationBean r");
+        return  request.getResultList();
+    }
+
+    @Override
+    public ReservationBean getReservation(Integer id) {
+        return em.find(ReservationBean.class, id);
+    }
 }
